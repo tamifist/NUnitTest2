5f818e5ab065a355437cca93254f80305a40a45b
tamifist <tamifist@gmail.com>
2021-03-10 20:52:48 +0300

    test

---
+++ UnitTestProject1/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+
+namespace UnitTestProject1
+{
+    public class UnitTest1
+    {
+        [SetUp]
+        public void Setup() { }
+
+        [Test]
+        public void TestMethod1()
+        {
+            NUnit.Framework.Assert.Pass();
+        }
+    }
+}
